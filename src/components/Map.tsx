aba56ea899c9a00f85ba47ff8eaa89421c7e5e60
Ezra Chai <juanzhe2@gmail.com>
2025-08-13 01:39:59 +0800

    select route page

---
+++ src/components/Map.tsx
@@ -0,0 +1,26 @@
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+// import "./App.css";
+import "leaflet/dist/leaflet.css";
+import { useSearchParams } from "react-router";
+
+function App() {
+  let [searchParams] = useSearchParams();
+
+  console.log(searchParams.get("code"));
+  return (
+    <>
+      <MapContainer
+        center={[5.4164, 100.3327]}
+        zoom={13.5}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import Map from "./components/Map.tsx";
+import SelectRoute from "./components/SelectRoute.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SelectRoute />} />
+          <Route index path="/map" element={<Map />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>
+);
