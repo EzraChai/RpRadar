1a9f0855b251de33f6dfc7017b7ed5c908d3735e
ezrachai <juanzhe2@gmail.com>
2025-08-20 18:25:04 +0800

    feat(Map): Adjust StopsCard layout for improved responsiveness and spacing

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -73,10 +73,10 @@ function App() {
             map.scrollWheelZoom.enable();
             map.dragging.enable();
           }}
-          className="absolute z-[1000] py-0 overflow-hidden gap-0 max-w-1/6 scroll-smooth bottom-8 backdrop-blur-lg border-white dark:border-neutral-500 bg-white/50 dark:bg-white/10 right-4  shadow-md h-1/2 "
+          className="absolute z-[1000] py-0 overflow-hidden gap-0 max-w-1/5 scroll-smooth bottom-8 backdrop-blur-lg border-white dark:border-neutral-500 bg-white/50 dark:bg-white/10 right-4  shadow-md h-1/2 "
         >
           <CardTitle className="space-y-2 px-6 py-6">
-            <h2 className="font-bold flex justify-between items-center gap-4">
+            <h2 className="font-bold flex justify-between items-center gap-2">
               <div className="text-md flex justify-center items-center font-bold border-2 w-12 h-6 py-1 px-2 border-red-500 rounded-xl">
                 {route?.route_short_name}
               </div>
