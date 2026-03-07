15c46e605aa7875440d3ba44a0c0e8e51646af11
ezrachai <juanzhe2@gmail.com>
2026-03-08 00:40:07 +0800

    fix: remove debug log from nextBusTime function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -43,7 +43,6 @@ export function hasCurrentTimePassed(time: string): boolean {
 }
 
 export function nextBusTime(times: string[] | undefined) {
-  console.log(times);
   if (times === undefined) {
     return null;
   }
